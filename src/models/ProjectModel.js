04d11326e46246c05ab47d40b108806ff8bde066
Charles Caleb <charlesofficial9@gmail.com>
2025-03-18 14:19:48 +0100

    update

--- src/models/ProjectModel.js
+++ src/models/ProjectModel.js
@@ -1,42 +1,50 @@
 // models/Project.js
 const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
-  project_title: { type: String, required: true, maxlength: 200 },
-  donor: { type: String, required: true, maxlength: 50 },
-  project_partners: [{ type: String }],
-  project_code: { type: String, required: true, maxlength: 50 },
-  implementation_period: {
-    from: { type: String, required: true },
-    to: { type: String, required: true },
-  },
-  project_budget: { type: String, required: true },
-  sectors: [
-    {
-      name: {
-        type: String,
-        enum: [
-          "Education",
-          "Protection",
-          "WASH",
-          "Nutrition/Health",
-          "Livelihood",
-        ],
-        required: true,
-      },
-      percentage: { type: Number, min: 0, max: 100, required: true },
+const projectSchema = new mongoose.Schema(
+  {
+    project_title: { type: String, required: true, maxlength: 200 },
+    donor: { type: String, required: true, maxlength: 50 },
+    project_partners: [{ type: String }],
+    project_code: { type: String, required: true, maxlength: 50 },
+    implementation_period: {
+      from: { type: String, required: true },
+      to: { type: String, required: true },
     },
-  ],
-  project_locations: [{ type: String }],
-  target_beneficiaries: {
-    women: { type: Number, required: true },
-    girls: { type: Number, required: true },
-    boys: { type: Number, required: true },
-    men: { type: Number, required: true },
+    project_budget: { type: Number, required: true },
+    sectors: [
+      {
+        name: {
+          type: String,
+          enum: [
+            "Education",
+            "Protection",
+            "WASH",
+            "Nutrition/Health",
+            "Livelihood",
+          ],
+          required: true,
+        },
+        percentage: { type: Number, min: 0, max: 100, required: true },
+      },
+    ],
+    project_locations: [{ type: String }],
+    target_beneficiaries: {
+      women: { type: Number, required: true },
+      girls: { type: Number, required: true },
+      boys: { type: Number, required: true },
+      men: { type: Number, required: true },
+    },
+    project_objectives: { type: String, required: true, maxlength: 400 },
+    project_summary: { type: String, required: true, maxlength: 4000 },
+    status: {
+      type: String,
+      enum: ["ongoing", "completed", "cancelled"],
+      default: "ongoing",
+    },
   },
-  project_objectives: { type: String, required: true, maxlength: 400 },
-  project_summary: { type: String, required: true, maxlength: 4000 },
-});
+  { timestamps: true }
+);
 
 const Project = mongoose.model("Project", projectSchema);
 
