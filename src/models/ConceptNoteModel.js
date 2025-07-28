9111a83f88d44f6d369be2219371f736db266186
Charles Caleb <charlesofficial9@gmail.com>
2025-07-28 14:41:43 +0100

    update

--- src/models/ConceptNoteModel.js
+++ src/models/ConceptNoteModel.js
@@ -59,11 +59,13 @@ const conceptNoteSchema = new mongoose.Schema(
   }
 );
 
-conceptNoteSchema.set("toJSON", {
+userSchema.set("toJSON", {
   virtuals: true,
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id?.toString?.() || null;
-    delete returnedObject._id;
+    if (returnedObject._id) {
+      returnedObject.id = returnedObject._id.toString();
+      delete returnedObject._id;
+    }
     delete returnedObject.__v;
   },
 });
